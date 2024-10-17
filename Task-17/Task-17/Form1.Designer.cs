0bb6849f3557165fff1109cfb6bdc58089a251a4
Shmadikan <122324ediik@mail.ru>
2024-10-18 02:08:18 +0300

    Добавлено решение 17 задачи

---
+++ Task-17/Task-17/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace Task_17
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.zedGraphControl1 = new ZedGraph.ZedGraphControl();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // zedGraphControl1
+            // 
+            this.zedGraphControl1.Location = new System.Drawing.Point(2, 2);
+            this.zedGraphControl1.Name = "zedGraphControl1";
+            this.zedGraphControl1.ScrollGrace = 0D;
+            this.zedGraphControl1.ScrollMaxX = 0D;
+            this.zedGraphControl1.ScrollMaxY = 0D;
+            this.zedGraphControl1.ScrollMaxY2 = 0D;
+            this.zedGraphControl1.ScrollMinX = 0D;
+            this.zedGraphControl1.ScrollMinY = 0D;
+            this.zedGraphControl1.ScrollMinY2 = 0D;
+            this.zedGraphControl1.Size = new System.Drawing.Size(553, 426);
+            this.zedGraphControl1.TabIndex = 0;
+            this.zedGraphControl1.UseExtendedPrintDialog = true;
+            this.zedGraphControl1.Load += new System.EventHandler(this.zedGraphControl1_Load);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "get",
+            "set",
+            "add",
+            "indexAdd",
+            "remove"});
+            this.comboBox1.Location = new System.Drawing.Point(643, 146);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(145, 21);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(643, 127);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Выбор операции";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(646, 283);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(99, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Запустить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(646, 349);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "label2";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.zedGraphControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private ZedGraph.ZedGraphControl zedGraphControl1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Task-17/task-4/task-4/task-4/Program.cs
@@ -0,0 +1,417 @@
+﻿using System;
+using System.ComponentModel;
+using System.Linq.Expressions;
+using Task4;
+
+
+
+    namespace Task4
+    {
+        public class MyArrayList<T>
+        {
+            int size;
+            public T[] elementData = null;
+
+            public MyArrayList()
+            {
+                size = 0;
+            }
+
+            public MyArrayList(T[] a)
+            {
+                if (a.Length == 0)
+
+                    return;
+                elementData = new T[a.Length * 2];
+                for (int i = 0; i < a.Length; i++)
+                {
+                    elementData[i] = a[i];
+                    ++size;
+                }
+
+            }
+
+            public MyArrayList(int cap)
+            {
+                elementData = new T[cap];
+                size = 0;
+            }
+
+            public void AddElement(T e)
+            {
+                /// Добавление элемента в конец.
+
+                if (elementData == null) return;
+                size += 1;
+                if (size > elementData.Length)
+                {
+                    ReSize();
+                }
+                elementData[size - 1] = e;
+            }
+
+
+            public void Clear()
+            {
+                Array.Clear(elementData, 0, size - 1);
+                size = 0;
+            }
+
+
+            public bool Contains(object o)
+            {
+                ///Проверка на содержание элемента.
+
+                T oConverter = (T)o;
+                foreach (T e in elementData)
+                    if (oConverter.Equals(e))
+                        return true;
+
+                return false;
+            }
+
+            public bool ContainsAll(T[] a)
+            {
+                /// Метод, проверяющий наличие всех элементов
+                /// Из передаваемого массива в динамическом.
+
+
+
+                foreach (T e in a)
+                {
+                    bool flag = false;
+                    foreach (T t in elementData)
+                        if (t.Equals(e) == true)
+                            flag = true;
+                    if (!flag) return false;
+                }
+                return true;
+            }
+
+
+            public bool IsEmpty() => size == 0;
+
+
+            public void Remove(object o)
+            {
+                /// Метод, удаляющий первое включение объекта о.
+
+
+                T oConverter = (T)o;
+                if (size > 0)
+                {
+                    for (int i = 0; i < elementData.Length; i++)
+                    {
+                        if (elementData[i].Equals(o))
+                        {
+                            if (i == elementData.Length - 1)
+                            {
+                                size -= 1; return;
+                            }
+                            for (int j = 0; j < elementData.Length - 1; j++)
+                            {
+                                if (j >= i)
+                                    elementData[j] = elementData[j + 1];
+                            }
+                            size -= 1;
+                            return;
+                        }
+                    }
+                }
+            }
+
+
+            public void RemoveAll(T[] a)
+            {
+                /// Метод, удаляющий все элементы из динамического массива,
+                /// При условии, что этот элемент есть в передаваемом массиве.
+
+                if (size > 0)
+                {
+                    foreach (T o in a)
+                        for (int i = 0; i < elementData.Length; i++)
+                        {
+                            if (elementData[i].Equals(o))
+                            {
+                                if (i == elementData.Length - 1)
+                                {
+                                    size -= 1; return;
+                                }
+                                for (int j = 0; j < elementData.Length - 1; j++)
+                                {
+                                    if (j >= i)
+                                        elementData[j] = elementData[j + 1];
+                                }
+                                size -= 1;
+
+                            }
+                        }
+                }
+
+
+
+
+
+
+            }
+
+
+            public void RetainAll(T[] a)
+            {
+                /// Метод, который оставляет в дин. массиве элементы из а.
+
+
+
+                if (size > 0)
+                {
+                    for (int i = 0; i < size;)
+                    {
+                        if (ElInArray(elementData[i]) == false)
+                        {
+                            if (i == size - 1)
+                                size -= 1;
+                            else
+                            {
+                                for (int index = i; index < size; index++)
+                                {
+                                    elementData[index] = elementData[index + 1];
+                                }
+                                size -= 1;
+                            }
+
+                        }
+                        else i++;
+
+                    }
+
+
+
+
+
+
+                }
+
+                bool ElInArray(T el)
+                {
+                    /// Вспомогательный метод, для проверки наличия элемента.
+
+                    bool flag = false;
+                    for (int i = 0; i < a.Length; i++)
+                        if (a[i].Equals(el))
+                            flag = true;
+                    return flag;
+
+                }
+            }
+
+
+            public int Size() => size;
+
+            public T[] ToArray()
+            {
+                /// Метод возвращает массив из элементов дин. массива.
+
+                T[] arrayToReturn = new T[size];
+                for (int i = 0; i < size; i++)
+                {
+                    arrayToReturn[i] = elementData[i];
+                }
+                return arrayToReturn;
+            }
+
+            public T[] ToArray(T[] a = null)
+            {
+                /// Метод, который добавляет в массив а, все элементы из дин. массива.
+                /// Если передаваемый массив пустой, то возвращает все элементы дин массива.
+
+                if (a == null)
+                {
+                    T[] Ret = new T[size];
+                    for (int i = 0; i < size; i++)
+                        Ret[i] = elementData[i];
+                    return Ret;
+                }
+
+
+                T[] RetArray = new T[a.Length + size];
+                int index = 0;
+                for (; index < a.Length; index++)
+                    RetArray[index] = a[index];
+
+                for (int j = 0; j < size; j++)
+                {
+                    RetArray[index] = elementData[j];
+                    index += 1;
+                }
+                return RetArray;
+            }
+
+
+            public void Add(int index, T e0)
+            {
+                /// Добавление элемента по индексу.
+                if (index > size - 1 || index < 0)
+                    throw new IndexOutOfRangeException();
+
+                if (size + 1 > elementData.Length)
+                    ReSize();
+
+                T saveIndex = elementData[index];
+                elementData[index] = e0;
+                for (int i = index + 1; i < elementData.Length; i++)
+                {
+                    T saveEl = elementData[i];
+                    elementData[i] = saveIndex;
+                    saveIndex = saveEl;
+                }
+                ++size;
+            }
+
+            public void Add(int index, T[] elt)
+            {
+                /// Вставка элементов массива, начиная с индекса.
+
+                if (index > size - 1 || index < 0)
+                    throw new IndexOutOfRangeException();
+
+                while (size + elt.Length > elementData.Length)
+                    ReSize();
+
+                T[] newArray = new T[size - index];
+                for (int i = index, j = 0; j < size - index; i++, j++)
+                    newArray[j] = elementData[i];
+                int it = index;
+                for (int j = 0; j < elt.Length; j++)
+                {
+                    elementData[it] = elt[j];
+                    it++;
+                }
+                size = size + elt.Length;
+                for (int k = 0; k < newArray.Length; it++, k++)
+
+                    elementData[it] = newArray[k];
+
+
+
+
+
+
+
+            }
+
+
+
+            public T get(int index)
+            {
+                if (index > size - 1 || index < 0)
+                    throw new IndexOutOfRangeException();
+
+                return elementData[index];
+
+            }
+
+            public int IndexOf(object o)
+            {
+                /// Возвращает индекс первого включения объекта о.
+
+                T el = (T)o;
+                for (int i = 0; i < elementData.Length; i++)
+                    if (elementData[i].Equals(el))
+                        return i;
+                return -1;
+            }
+
+
+            public int LastIndex(object o)
+            {
+                /// Возвращает индекс последнего включения объекта о.
+
+                T el = (T)o;
+                for (int i = size - 1; i >= 0; i--)
+                {
+                    if (elementData[i].Equals(el))
+                        return i;
+                }
+                return -1;
+            }
+
+
+            public T ReturnRemove(int index)
+            {
+                /// Метод удаляет элемент, и возвращает его значение пользователю.
+                if (index > size - 1 || index < 0)
+                    throw new IndexOutOfRangeException();
+
+                if (index == size - 1)
+                {
+                    T retEl = elementData[size - 1];
+                    size -= 1;
+                    return retEl;
+                }
+                T el = elementData[index];
+                for (int i = index; i < size; i++)
+                    elementData[i] = elementData[i + 1];
+                size -= 1;
+
+
+                return el;
+            }
+
+
+            public void Set(int index, T e)
+            {
+                /// Замена по индексу.
+
+                if (index > size - 1 || index < 0)
+                    throw new IndexOutOfRangeException();
+                elementData[index] = e;
+            }
+
+
+            public T[] SubList(int fromindex, int toindex)
+            {
+                if (fromindex > toindex || fromindex < 0 || toindex >= size)
+                    throw new IndexOutOfRangeException();
+                /// Возвращение подмассива с элементами, начиная с первого
+                /// переданного индекса, до последнего.
+
+                T[] arr = new T[toindex - fromindex + 1];
+                int k = 0;
+                for (int i = fromindex; i <= toindex; i++)
+                {
+                    arr[k] = elementData[i];
+                    k++;
+                }
+                return arr;
+            }
+
+            public void ReSize()
+            {
+                /// Вспомогательный метод для перевыделения памяти.
+
+                if (size > elementData.Length)
+                {
+
+                    T[] NewArray = new T[elementData.Length * 2];
+                    for (int i = 0; i < elementData.Length; i++)
+                        NewArray[i] = elementData[i];
+                    elementData = NewArray;
+                }
+            }
+
+            public void Print()
+            {
+                /// Отладочный метод для просмотра элементов массива.
+
+                if (elementData != null)
+                {
+                    for (int i = 0; i < size; i++)
+                        Console.Write($"{elementData[i]} ");
+                }
+            }
+        }
+    }
+
+
+
+    
