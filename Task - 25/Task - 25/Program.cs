174fecf3cc18d083a4b428570e4ae432d0f178ae
Shmadikan <122324ediik@mail.ru>
2024-10-22 00:44:05 +0300

    25 задача

---
+++ Task - 25/Task - 25/Program.cs
@@ -0,0 +1,47 @@
+﻿using Task_23;
+namespace Task___25
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyHashSet<string> set = new MyHashSet<string>();
+            string path = "input.txt";
+            StreamReader stream = new StreamReader(path);
+            string line = "";
+            line = stream.ReadLine();
+            while (line != null) { 
+                
+                string word = "";
+                bool flag = false;
+                foreach (char sym in line) {
+                    if (Char.IsLetter(sym))
+                    {
+                        word += sym.ToString().ToLower();
+                        flag = true;
+                    }
+                    else
+                    {
+                        if (flag)
+                        {
+                            set.Add(word);
+                            word = "";
+                            flag = false;
+                        }
+                    }
+                }
+                
+                line = stream.ReadLine();
+            }
+            string[] strings = set.ToArray();
+            foreach (string s in strings)
+            {
+                Console.WriteLine(s);
+            }
+        
+        }
+
+
+
+    }
+}
